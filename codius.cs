b9a02d77ec982996ba56f0622c60d66101c98d28
Antony Carlie <161594393+siriushorn@users.noreply.github.com>
2024-02-28 13:55:43 +0200

    Update codius.cs

--- codius.cs
+++ codius.cs
@@ -18,3 +18,15 @@
 
     if (System.Diagnostics.Debugger.IsAttached)
         return true;
+  if (value.IsEmpty() || !value.Contains(path))
+    {
+        value = value.EmptyNull().Trim(';') + ';' + path;
+        Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.Process);
+    }
+}
+
+void SetupLogging(ILoggingBuilder loggingBuilder)
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.AddSerilog();
+}
